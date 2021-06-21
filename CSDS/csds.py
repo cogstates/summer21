794560cfa099ffc3fa05e29b19d4e9afa9a7c625
owenrambow-stonybrook <owen.rambow@stonybrook.edu>
2021-06-21 18:10:59 -0400

    Initial version of Cognitive State Data Structure (CSDS)

---
+++ CSDS/csds.py
@@ -0,0 +1,28 @@
+# This is the Cognitive States Data Structure (CSDS) which represents information about writer cognitive state and
+# the text from which we have gleaned this information.
+
+# Basic information per instance of CognitiveStateFromText:
+# - the text
+# - the proposition in the text about which we record the cognitive state;
+#     we represent this by the start and end of the syntactic headword of the
+#     proposition
+# - the belief value; the values used are corpus-specific (or experiment-specific),
+#     for example CB, NCB, ROB, NA
+# - the sentiment value; the values used are corpus-specific (or experiment-specific),
+#     for example pos, neg
+
+# Many more fields will be added as we go along.
+
+
+class CognitiveStateFromText:
+    text = ""  # sentence
+    head_start = -1  # offset of start of head word of proposition
+    head_end = -1  # offset of end of head word of proposition
+    belief = ""  # belief value (values are corpus-specific)
+    sentiment = ""  # sentiment value (values are corpus-specific)
+
+    def __init__(self, this_text, this_head_start, this_head_end, this_belief):
+        text = this_text
+        head_start = this_head_start
+        head_end = this_head_end
+        belief = this_belief
