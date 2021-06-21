20b8ce34a93a1efb5ac722e44d17c47a404c8f3e
Amittai-Aviram <50280989+Amittai-Aviram@users.noreply.github.com>
2021-06-21 15:25:46 -0400

    Add files via upload
    Script containing all the steps of the Hugging Face Transformers fine-tuning tutorial.

---
+++ imdb.py
@@ -0,0 +1,36 @@
+from datasets import load_dataset, load_metric
+import numpy as np
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+
+raw_datasets = load_dataset("imdb")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+full_train_dataset = tokenized_datasets["train"]
+full_eval_dataset = tokenized_datasets["test"]
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+metric = load_metric("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+training_args = TrainingArguments("test_trainer")
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=full_train_dataset,
+    eval_dataset=full_eval_dataset,
+    compute_metrics=compute_metrics,
+)
+results = trainer.evaluate()
+print(results)
+
