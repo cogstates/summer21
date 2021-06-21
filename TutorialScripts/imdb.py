46043df1aebe1910f23e1b2673d1bf2d31919c68
Amittai-Aviram <50280989+Amittai-Aviram@users.noreply.github.com>
2021-06-21 16:20:45 -0400

    Update imdb.py
    Added comments and instructions.

--- TutorialScripts/imdb.py
+++ TutorialScripts/imdb.py
@@ -1,3 +1,11 @@
+# imdb.py
+# Script containing all the steps of the Hugging Face Transformers fine-tuning tutorial.
+# Run thus in a virtual environment containing PyTorch, Transformers, Datasets, and sklearn.
+# $ python imdb.py
+# For the full data set, with GPUs available, this should take about 2 hours and 45 minutes to complete.
+# The smaller data set (see comments below) takes only about 7 minutes and 20 seconds to complete.
+# Author: Amittai Aviram - aviram@bc.edu
+
 from datasets import load_dataset, load_metric
 import numpy as np
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
@@ -18,6 +26,10 @@ full_eval_dataset = tokenized_datasets["test"]
 model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
 metric = load_metric("accuracy")
 
+# In the named arguments below, replace full_train_dataset
+# and full-eval_dataset with small_train_dataset and
+# small_eval_dataset, respectively, for experimentation with
+# a small subset of the input data and a shorter running time.
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
