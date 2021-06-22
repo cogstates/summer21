994842105d4c38bdcc1f52063a32a3e69c4eb4d2
Amittai Aviram <amittai.aviram@gmail.com>
2021-06-22 16:43:38 -0400

    Adjusted vertical spaces to comply with PEP.

--- TutorialScripts/imdb.py
+++ TutorialScripts/imdb.py
@@ -16,6 +16,7 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True)
 
+
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
 
 small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
@@ -34,6 +35,7 @@ def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
+
 
 training_args = TrainingArguments("test_trainer")
 trainer = Trainer(
