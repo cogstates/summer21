877b64e29d0c9d4607dbd7ceb2c98e9f25470123
erinye <erin.lynn.ye@gmail.com>
2021-07-07 15:49:00 -0400

    attempt to parse data

--- xml2csds/2010data.py
+++ xml2csds/2010data.py
@@ -1,3 +1,11 @@
-#this will transform xml into csds
-#trying again
-#last time
+# testing out ElementTree parsing
+
+import xml.etree.ElementTree as ET
+in_file = '/Users/erinye/Desktop/20000415_apw_eng-New.xml'
+tree = ET.parse(in_file)
+text_with_nodes = tree.find('TextWithNodes')
+print(text_with_nodes.text)
+for node in text_with_nodes.findall('Node'):
+    print(node.attrib['id'], node.tail.replace('\n', ''))
+
+
