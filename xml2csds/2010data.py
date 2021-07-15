ea355c362f5d9dff24692b13fb35f346760c6ec2
erinye <erin.lynn.ye@gmail.com>
2021-07-15 12:35:15 -0400

    set up CSDS dictionary

--- xml2csds/2010data.py
+++ xml2csds/2010data.py
@@ -1,11 +1,34 @@
-# testing out ElementTree parsing
-# need to figure out how to reset offsets for every new line
 import xml.etree.ElementTree as ET
+
+from CSDS.csds import CognitiveStateFromText
+
 in_file = '/Users/erinye/Desktop/20000415_apw_eng-New.xml'
 tree = ET.parse(in_file)
+
 text_with_nodes = tree.find('TextWithNodes')
 print(text_with_nodes.text)
+list = [text_with_nodes.text]
 for node in text_with_nodes.findall('Node'):
-    print(node.attrib['id'], node.tail.replace('\n', ''))
+    list.append(node.tail.replace('\n', ''))
+
+annotation_sets = tree.findall('AnnotationSet')
+print("length:", len(annotation_sets))
+for annotation_set in annotation_sets:
+    for node in annotation_set:
+        print(node.attrib['StartNode'], node.attrib['EndNode'], node.attrib['Type'])
+
+c = 0
+csds = {}
+for annotation in annotation_sets:
+    for node in annotation:
+        while c < len(list):
+            cd = CognitiveStateFromText(list[c], node.attrib['StartNode'], node.attrib['EndNode'], node.attrib['Type'], '')
+            csds[c] = cd
+            c += 1
+
+# still need to check that StartNode - EndNode = length of snippet
+# still need to synthesize sentences
+# still need to change offsets so that they start with every new line
+
 
 
