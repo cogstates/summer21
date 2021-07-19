086c2cb82a8129577d2e6b9602d59d62aead1958
erinye <erin.lynn.ye@gmail.com>
2021-07-19 10:18:06 -0400

    updated code to match new csds

--- xml2csds/2010data.py
+++ xml2csds/2010data.py
@@ -5,10 +5,10 @@ in_file = '/Users/erinye/Desktop/20000415_apw_eng-New.xml'
 tree = ET.parse(in_file)
 
 text_with_nodes = tree.find('TextWithNodes')
-print(text_with_nodes.text)
 list = [text_with_nodes.text]
 for node in text_with_nodes.findall('Node'):
     list.append(node.tail.replace('\n', ''))
+print(list)
 
 annotation_sets = tree.findall('AnnotationSet')
 print("length:", len(annotation_sets))
@@ -16,18 +16,20 @@ for annotation_set in annotation_sets:
     for node in annotation_set:
         print(node.attrib['StartNode'], node.attrib['EndNode'], node.attrib['Type'])
 
-b = CSDS("No text corpus")
+ds = CSDS("No text corpus")
 c = 0
 new = {}
 for annotation in annotation_sets:
     for node in annotation:
         while c < len(list):
-            b.instances.append(CognitiveStateFromText(list[c], node.attrib['StartNode'], node.attrib['EndNode'], node.attrib['Type']))
+            if not len(list[c]) == ((int(node.attrib['StartNode'])) - (int(node.attrib['EndNode']))):
+                print('ERROR: offsets do not match length of snippet')
+            ds.instances.append(CognitiveStateFromText(list[c], node.attrib['StartNode'], node.attrib['EndNode'], node.attrib['Type']))
             c += 1
-print(b.get_info_long())
-# still need to check that StartNode - EndNode = length of snippet
-# still need to synthesize sentences
+print(ds.get_info_long)
+
 # still need to change offsets so that they start with every new line
+# still need to add to dictionary?
 
 
 
