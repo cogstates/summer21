f6c84a48d6e9eaa2dd47f7ce91a07b11307cddca
Amittai Aviram <amittai.aviram@gmail.com>
2021-07-21 14:17:35 -0400

    Rearranged code and added more test calls.

--- CSDS/sample_csds.py
+++ CSDS/sample_csds.py
@@ -1,15 +1,33 @@
-# This file creates a sample instance of a CSDS to run tests.
+# This file creates a sample instance of a CSDS to run tests
+# and serves as a unit test of the CSDS API.
+
 from csds import CognitiveStateFromText, CSDS
 
-sample_csds = CSDS("No text corpus")
+sample_corpus = [
+    ("John said he likes beets.", 5, 9, "CB"),
+    ("John said he likes beets.", 13, 18, "NCB"),
+    ("Mary sometimes says she likes beets.", 15, 19, "CB"),
+    ("Mary sometimes says she likes beets.", 25, 29, "NCB"),
+    ("Maybe Mulligan said she likes beets.", 15, 19, "NCB"),
+    ("Maybe Mulligan said she likes beets.", 25, 29, "NCB"),
+]
 
-sample_csds.instances.append(CognitiveStateFromText("John said he likes beets.", 5, 9, "CB"))
-sample_csds.instances.append(CognitiveStateFromText("John said he likes beets.", 13, 18, "NCB"))
-sample_csds.instances.append(CognitiveStateFromText("Mary sometimes says she likes beets.", 15, 19, "CB"))
-sample_csds.instances.append(CognitiveStateFromText("Mary sometimes says she likes beets.", 25, 29, "NCB"))
-sample_csds.instances.append(CognitiveStateFromText("Maybe Mulligan said she likes beets.", 15, 19, "NCB"))
-sample_csds.instances.append(CognitiveStateFromText("Maybe Mulligan said she likes beets.", 25, 29, "NCB"))
 
-print("Created sample CSDS instance")
-print(sample_csds.get_info_short())
-print(sample_csds.get_info_long())
+def make_cognitive_state(sample_tuple):
+    return CognitiveStateFromText(*sample_tuple)
+
+
+if __name__ == "__main__":
+    sample_csds = CSDS("No text corpus")
+    for sample in sample_corpus:
+        sample_csds.add_instance(CognitiveStateFromText(*sample))
+    print("Created sample CSDS instance")
+    print(sample_csds.get_info_short())
+    print(sample_csds.get_info_long())
+    # Not something you would normally do--therefore not in the API:
+    sample_csds.instances.clear()
+    new_samples = list(map(make_cognitive_state, sample_corpus))
+    sample_csds.add_list_of_instances(new_samples)
+    print(sample_csds.get_info_short())
+    for sample in sample_csds.get_next_instance():
+        print(sample.text)
