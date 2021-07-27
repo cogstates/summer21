bf0d34810d361ae7af47fb4beb13df2df919ee19
Amittai Aviram <amittai.aviram@gmail.com>
2021-07-27 13:27:48 -0400

    Class docstring. Node to offset mapping.

--- xml2csds/xml2csds.py
+++ xml2csds/xml2csds.py
@@ -4,11 +4,17 @@ from CSDS.csds import CognitiveStateFromText, CSDSCollection
 
 
 class XMLCorpusToCSDSCollection:
+    """
+    Class to create a Cognitive State Data Structure (CSDS) collection
+    corresponding to a corpus consisting of XML files with annotations
+    on text targets (heads) following the GATE format.
+    """
     corpus_name = ""
     corpus_directory = ""
     csds_collection = None
     nodes_to_sentences = {}
     nodes_to_targets = {}
+    nodes_to_offsets = {}
 
     def __init__(self, corpus_name, corpus_directory):
         self.corpus_name = corpus_name
@@ -19,12 +25,14 @@ class XMLCorpusToCSDSCollection:
         text_with_nodes = tree.find('TextWithNodes')
         nodes_in_sentence = []
         sentence = text_with_nodes.text
+        sentence_length_so_far = len(sentence)
         self.nodes_to_targets[0] = text_with_nodes.text
         for node in text_with_nodes.findall('Node'):
             text = node.tail
             node_id = node.attrib['id']
             self.nodes_to_targets[node_id] = text
             nodes_in_sentence.append(node_id)
+            self.nodes_to_offsets[node_id] = sentence_length_so_far
             if '\n' in text:
                 parts = text.split('\n')
                 sentence += parts[0]
@@ -34,11 +42,14 @@ class XMLCorpusToCSDSCollection:
                 sentence = parts[-1]
             else:
                 sentence += text
+                sentence_length_so_far += len(text)
 
     def add_file_to_csds_collection(self, tree):
         annotation_sets = tree.findall('AnnotationSet')
         for annotation_set in annotation_sets:
             for annotation in annotation_set:
+                # Do the necessary arithmetic here to get the head_end.
+                # Use that value for the head_end argument to the cSDS constructor.
                 cog_state = CognitiveStateFromText(self.nodes_to_sentences[annotation.attrib['StartNode']],
                                                    annotation.attrib['StartNode'],
                                                    annotation.attrib['EndNode'],
@@ -46,7 +57,6 @@ class XMLCorpusToCSDSCollection:
                                                    self.nodes_to_targets[annotation.attrib['StartNode']]
                                                    )
                 self.csds_collection.add_instance(cog_state)
-                # print(cog_state.get_info_short())
 
     def add_file(self, xml_file):
         tree = et.parse(xml_file)
@@ -58,7 +68,6 @@ class XMLCorpusToCSDSCollection:
     def create_and_get_collection(self):
         for file in glob.glob(self.corpus_directory + '/*.xml'):
             self.add_file(file)
-        self.add_file('./20000415_apw_eng-New.xml')
         return self.csds_collection
 
 
