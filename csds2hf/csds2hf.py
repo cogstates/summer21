fdafe3ef475bcecc475b2cf1b5402bd880dc5cf0
Amittai Aviram <amittai.aviram@gmail.com>
2021-08-11 12:22:15 -0400

    Modified script.py and took script code out of csds2hf.py.

--- csds2hf/csds2hf.py
+++ csds2hf/csds2hf.py
@@ -1,6 +1,4 @@
 from datasets import Dataset, DatasetDict, ClassLabel, load_metric
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
-from xml2csds.xml2csds import XMLCorpusToCSDSCollection
 
 
 class CSDS2HF:
@@ -41,43 +39,3 @@ class CSDS2HF:
         return DatasetDict({'train': csds_train_dataset, 'eval': csds_test_dataset})
 
 
-def notify(string):
-    print(">>>>   ", string, "   <<<<")
-
-
-def tokenize_function(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
-
-
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
-
-
-if __name__ == '__main__':
-    input_processor = XMLCorpusToCSDSCollection(
-        '2010 Language Understanding',
-        '../CMU')
-    collection = input_processor.create_and_get_collection()
-    csds2hf = CSDS2HF(collection)
-    csds_datasets = csds2hf.get_dataset_dict()
-    notify("Created dataset, now tokenizing dataset")
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-    tokenized_csds_datasets = csds_datasets.map(tokenize_function, batched=True)
-    notify("Done tokenizing dataset")
-    model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=5)
-    metric = load_metric("accuracy")
-    notify("Starting training")
-    training_args = TrainingArguments("../CSDS/test_trainer")
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_csds_datasets['train'],
-        eval_dataset=tokenized_csds_datasets['eval'],
-        compute_metrics=compute_metrics,
-    )
-    trainer.train()
-    notify("Done training")
-    results = trainer.evaluate()
-    print(results)
