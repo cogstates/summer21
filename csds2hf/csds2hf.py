e21299bd8782097c58830311ee2b82f734c6e4a2
Amittai Aviram <amittai.aviram@gmail.com>
2021-08-12 15:58:07 -0400

    Restored correct version of csds2hf.py.

--- csds2hf/csds2hf.py
+++ csds2hf/csds2hf.py
@@ -1,9 +1,4 @@
-
-
-import datasets
-from CSDS.csds import CSDS, CSDSCollection
 from datasets import Dataset, DatasetDict, ClassLabel, load_metric
-from xml2csds.xml2csds import XMLCorpusToCSDSCollection
 
 
 class CSDS2HF:
@@ -12,10 +7,8 @@ class CSDS2HF:
     test_text = []
     training_labels = []
     test_labels = []
+    unique_labels = []
     csds_collection = None
-
-    # First create a mapping from string labels to integers
-    cl = ClassLabel(num_classes=3, names=['CB', 'NCB', 'NA'])
 
     def __init__(self, csds_collection):
         self.csds_collection = csds_collection
@@ -26,46 +19,23 @@ class CSDS2HF:
         for instance in self.csds_collection.get_next_instance():
             text.append(instance.get_marked_text())
             beliefs.append(instance.get_belief())
+        self.unique_labels = list(set(beliefs))
         size = len(text)
         size_training = size - size // 4
         self.training_text = text[:size_training]
         self.test_text = text[size_training:]
         self.training_labels = beliefs[:size_training]
-        self.training_labels = self.convert_labels(self.training_labels)
         self.test_labels = beliefs[size_training:]
-        self.test_labels = self.convert_labels(self.test_labels)
 
-        csds_train_dict = self.make_dict(self.training_text, self.training_labels)
-        csds_eval_dict = self.make_dict(self.test_text, self.test_labels)
+    def get_dataset_dict(self):
+        self.populate_lists()
+        class_label = ClassLabel(num_classes=len(self.unique_labels), names=self.unique_labels)
+        csds_train_dataset = Dataset.from_dict(
+            {"text": self.training_text, "labels": list(map(class_label.str2int, self.training_labels))}
+        )
+        csds_test_dataset = Dataset.from_dict(
+            {"text": self.test_text, "labels": list(map(class_label.str2int, self.test_labels))}
+        )
+        return DatasetDict({'train': csds_train_dataset, 'eval': csds_test_dataset})
 
-        print(csds_eval_dict)
-
-
-    def make_dict(self, text, labels):
-        csds_dict = {"text": text, "labels": map(cl.str2int, labels)}
-        return csds_dict
-
-
-    def convert_labels(self, labels):
-        i = 0
-        while i < len(labels):
-            if labels[i] == "Not Applicable":
-                labels[i] = "NA"
-                i += 1
-            elif labels[i] == "Non-Committed Belief":
-                labels[i] = "NCB"
-                i += 1
-            else:
-                labels[i] = "CB"
-                i += 1
-        return labels
-
-
-if __name__ == '__main__':
-    input_processor = XMLCorpusToCSDSCollection(
-        '2010 Language Understanding',
-        '../CMU')
-    collection = input_processor.create_and_get_collection()
-    csds2hf = CSDS2HF(collection)
-    csds2hf.populate_lists()
 
