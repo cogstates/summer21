c2c58ab67dfa63c48427724e797269de54407976
erinye <erin.lynn.ye@gmail.com>
2021-08-18 14:35:38 -0400

    updated populate_labels function

--- csds2hf/csds2hf.py
+++ csds2hf/csds2hf.py
@@ -13,19 +13,54 @@ class CSDS2HF:
     def __init__(self, csds_collection):
         self.csds_collection = csds_collection
 
+    # splits data into train and test
     def populate_lists(self):
-        text = []
-        beliefs = []
-        for instance in self.csds_collection.get_next_instance():
-            text.append(instance.get_marked_text())
-            beliefs.append(instance.get_belief())
-        self.unique_labels = list(set(beliefs))
-        size = len(text)
+        # checks example sentences in each document
+        doc_id_count_table = {}
+        size = self.csds_collection.get_labeled_instances_length() + self.csds_collection.get_o_instances_length()
         size_training = size - size // 4
-        self.training_text = text[:size_training]
-        self.test_text = text[size_training:]
-        self.training_labels = beliefs[:size_training]
-        self.test_labels = beliefs[size_training:]
+        for instance in self.csds_collection.get_next_instance():
+            # add get_doc_id in CSDS
+            doc_id = instance.get_doc_id()
+            if doc_id in doc_id_count_table:
+                doc_id_count_table[doc_id] += 1
+            else:
+                doc_id_count_table[doc_id] = 1
+
+        # assigns train or testing to the documents
+        doc_id_train_or_test = {}
+        size_training_so_far = 0
+        size_check_sum = 0
+        for document in doc_id_count_table:
+            if size_training_so_far < size_training:
+                doc_id_train_or_test[document] = 'train'
+                size_training_so_far += doc_id_count_table[document]
+            else:
+                doc_id_train_or_test[document] = 'test'
+            size_check_sum += doc_id_count_table[document]
+        print('Size of training corpus:', size_training_so_far)
+        print('Percent of training text:', size_training_so_far // size)
+        if size != size_check_sum:
+            print('Warning: size does not check out')
+
+        for instance in self.csds_collection.get_next_instance():
+            # check doc ID and train vs test
+            # populate lists in loop
+            doc_id = instance.get_doc_id()
+            if doc_id_train_or_test[doc_id] == 'train':
+                self.training_text.append(instance.get_marked_text())
+                self.training_labels.append(instance.get_belief())
+            else:
+                self.test_text.append(instance.get_marked_text())
+                self.test_labels.append(instance.get_belief())
+
+        beliefs = []
+        beliefs += self.training_labels
+        beliefs += self.test_labels
+        self.unique_labels = list(set(beliefs))
+
+        if (len(self.test_labels) - len(self.training_labels)) > 0:
+            print('Error: unseen labels in test_labels')
 
     def get_dataset_dict(self):
         self.populate_lists()
