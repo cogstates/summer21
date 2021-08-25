aac49591084fa9a0252f6bbdfc5d95e778453132
Amittai Aviram <amittai.aviram@gmail.com>
2021-08-24 23:29:24 -0400

    Updated test code for CSDS and collection.

--- CSDS/sample_csds.py
+++ CSDS/sample_csds.py
@@ -4,30 +4,35 @@
 from csds import CSDS, CSDSCollection
 
 sample_corpus = [
-    ("John said he likes beets.", 5, 9, "CB"),
-    ("John said he likes beets.", 13, 18, "NCB"),
-    ("Mary sometimes says she likes beets.", 15, 19, "CB"),
-    ("Mary sometimes says she likes beets.", 24, 29, "NCB"),
-    ("Maybe Mulligan said she likes beets.", 15, 19, "NCB"),
-    ("Maybe Mulligan said she likes beets.", 24, 29, "NCB"),
+    ("John said he likes beets.", 5, 9, "CB", "said"),
+    ("John said he likes beets.", 13, 18, "NCB", "likes"),
+    ("Mary sometimes says she likes beets.", 15, 19, "CB", "says"),
+    ("Mary sometimes says she likes beets.", 24, 29, "NCB", "likes"),
+    ("Maybe Mulligan said she likes beets.", 15, 19, "NCB", "said"),
+    ("Maybe Mulligan said she likes beets.", 24, 29, "NCB", "likes"),
 ]
+
+sentence_id = 0
 
 
 def make_cognitive_state(sample_tuple):
-    return CSDS(*sample_tuple)
+    global sentence_id
+    csds = CSDS(*sample_tuple, 0, sentence_id)
+    sentence_id += 1
+    return csds
 
 
 if __name__ == "__main__":
-    sample_csds = CSDSCollection("No text corpus")
-    for sample in sample_corpus:
-        sample_csds.add_labeled_instance(CSDS(*sample))
-    print("Created sample CSDS instance")
-    print(sample_csds.get_info_short())
-    print(sample_csds.get_info_long())
+    sample_csds_collection = CSDSCollection("No text corpus")
+    for sentence_id, sample in enumerate(sample_corpus):
+        sample_csds_collection.add_labeled_instance(CSDS(*sample, 0, sentence_id))
+    print("Created sample CSDS collection")
+    print(sample_csds_collection.get_info_short())
+    print(sample_csds_collection.get_info_long())
     # Not something you would normally do--therefore not in the API:
-    sample_csds.labeled_instances.clear()
+    sample_csds_collection.labeled_instances.clear()
     new_samples = list(map(make_cognitive_state, sample_corpus))
-    sample_csds.add_list_of_labeled_instances(new_samples)
-    print(sample_csds.get_info_short())
-    for sample in sample_csds.get_next_instance():
+    sample_csds_collection.add_list_of_labeled_instances(new_samples)
+    print(sample_csds_collection.get_info_short())
+    for sample in sample_csds_collection.get_next_instance():
         print(sample.get_marked_text())
