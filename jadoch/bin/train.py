a0476feb7edc73b84502b2712b458b03f405a497
Adil Soubki <asoubki@login2.cm.cluster>
2021-10-14 20:36:27 -0400

    add a module for submitting jobs to slurm, update train.py

--- jadoch/bin/train.py
+++ jadoch/bin/train.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+import os
+import sys
 
 from datasets import DatasetDict
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
+
+from jadoch.core import slurm
 
 
 def compute_metrics(pred):
@@ -11,45 +15,60 @@ def compute_metrics(pred):
 
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average=None)
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels, preds, average=None
+    )
     acc = accuracy_score(labels, preds)
     return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall,
-        'preds': preds
+        "accuracy": acc,
+        "f1": f1,
+        "precision": precision,
+        "recall": recall,
+        "preds": preds,
     }
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("path")
+    parser.add_argument("-p", "--partition", default="gpu,gpu-long,gpu-large")
+    parser.add_argument("-l", "--local", action="store_true")
     args = parser.parse_args()
+
+    if sys.stdin.isatty() and not args.local:
+        return slurm.sbatch(
+            f"python -u {os.path.abspath(__file__)} {' '.join(sys.argv[1:])}",
+            flags=dict(partition=args.partition, exclude="sn-nvda8"),
+            modules=["cuda102/toolkit/10.2"],
+        ).returncode
 
     dataset = DatasetDict.load_from_disk(args.path)
     tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
     tokenized_dataset = dataset.map(
         lambda dt: tokenizer(dt["text"], padding="max_length", truncation=True),
-        batched=True
+        batched=True,
     )
 
-    model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "bert-base-cased", num_labels=2
+    )
     trainer = Trainer(
         model=model,
-        train_dataset=tokenized_dataset["train"], #.select(range(100)),
-        eval_dataset=tokenized_dataset["test"], #.select(range(100)),
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["test"],
         compute_metrics=compute_metrics,
     )
     trainer.train()
     results = trainer.evaluate()
     labels = ["na", "ja"]
     for idx, pred in enumerate(results["eval_preds"]):
-        print({
-            "text": tokenized_dataset["test"][idx]["text"],
-            "label": labels[tokenized_dataset["test"][idx]["label"]],
-            "prediction": labels[pred]
-        })
+        print(
+            {
+                "text": tokenized_dataset["test"][idx]["text"],
+                "label": labels[tokenized_dataset["test"][idx]["label"]],
+                "prediction": labels[pred],
+            }
+        )
     print(results)
 
 
---
+++ jadoch/jadoch/core/slurm.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+import sys
+from typing import Dict, Iterable, Optional
+
+from .functional import safe_iter
+
+
+_FLAGS = {
+    "job-name": os.path.basename(sys.argv[0]),
+    "output": "/gpfs/scratch/%u/%x.%j.out",
+    "partition": "medium-28core",
+}
+
+
+def sbatch(
+    cmds: Iterable[str],
+    flags: Optional[Dict[str, str]] = None,
+    modules: Optional[Iterable[str]] = None,
+) -> subprocess.CompletedProcess[bytes]:  # pylint: disable=unsubscriptable-object
+    # Parse inputs.
+    cmds = safe_iter(cmds)
+    flags = flags or _FLAGS
+    for key in _FLAGS:
+        if key not in flags:
+            flags[key] = _FLAGS[key]
+    modules = modules or []
+    # Prepare batch script.
+    stdin = ["#!/bin/bash"]
+    for key, val in flags.items():
+        stdin.append(f"#SBATCH --{key}={val}")
+    if modules:
+        stdin.append("")
+    for module in modules:
+        stdin.append(f"module load {module}")
+    stdin.append("")
+    stdin += cmds
+    print("\n".join(stdin))
+    # Submit the job.
+    return subprocess.run(
+        ["sbatch"],
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        input="\n".join(stdin).encode("utf-8"),
+        check=True,
+    )
