5da7a0aced86c0b76cd3b93ad4d22548f6043bf0
Tyler Osborne <tyler.922@hotmail.com>
2022-04-03 18:26:07 -0400

    fixing style in ddl

--- db_implementation/ddl.py
+++ db_implementation/ddl.py
@@ -9,41 +9,41 @@ cur = con.cursor()
 
 # DDL for tables
 def create_tables():
-    cur.execute('CREATE TABLE SENTENCES ('
+    cur.execute('CREATE TABLE sentences ('
                 'sentence_id INTEGER PRIMARY KEY AUTOINCREMENT,'
                 'file VARCHAR2(255) NOT NULL,'
                 'file_sentence_id INTEGER NOT NULL,'
-                'sentence varchar2(255) NOT NULL )')
+                'sentence VARCHAR2(255) NOT NULL )')
 
-    cur.execute('CREATE TABLE MENTIONS ('
+    cur.execute('CREATE TABLE mentions ('
                 'token_id INTEGER PRIMARY KEY AUTOINCREMENT,'
-                'sentence_id REFERENCES SENTENCES(sentence_id),'
+                'sentence_id REFERENCES sentences(sentence_id),'
                 'token_text VARCHAR2(255),'
                 'token_offset_start INTEGER,'
-                'token_offstart_end INTEGER,'
+                'token_offset_end INTEGER,'
                 'phrase_text VARCHAR2(255),'
                 'phrase_offset_start INTEGER,'
                 'phrase_offset_end INTEGER )')
 
 
-    cur.execute('CREATE TABLE SOURCES ('
+    cur.execute('CREATE TABLE sources ('
                 'source_id INTEGER PRIMARY KEY AUTOINCREMENT,'
-                'token_id REFERENCES MENTIONS(token_id),'
+                'token_id REFERENCES mentions(token_id),'
                 'nesting_level INTEGER,'
                 '[source] VARCHAR2(255) )')
 
-    cur.execute('CREATE TABLE ATTITUDES ('
+    cur.execute('CREATE TABLE attitudes ('
                 'attitude_id INTEGER PRIMARY KEY AUTOINCREMENT,'
-                'source_id REFERENCES SOURCES(source_id),'
-                'target_token_id REFERENCES MENTIONS(token_id),'
+                'source_id REFERENCES sources(source_id),'
+                'target_token_id REFERENCES mentions(token_id),'
                 'label VARCHAR2(255),'
                 'label_type VARCHAR2(255) )')
 
 def clear_database():
-    cur.execute('DROP TABLE ATTITUDES')
-    cur.execute('DROP TABLE SOURCES')
-    cur.execute('DROP TABLE MENTIONS')
-    cur.execute('DROP TABLE SENTENCES')
+    cur.execute('DROP TABLE attitudes')
+    cur.execute('DROP TABLE sources')
+    cur.execute('DROP TABLE mentions')
+    cur.execute('DROP TABLE sentences')
 
 # clear_database()
 create_tables()
