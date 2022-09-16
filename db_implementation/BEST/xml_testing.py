99af532e9cf52108eefc992a353c05f7d4daa102
Tyler Osborne <tyler.922@hotmail.com>
2022-09-15 22:31:53 -0400

    playing around with XML ElementTree to process BEST files.

---
+++ db_implementation/BEST/xml_testing.py
@@ -0,0 +1,19 @@
+# author: tyler osborne
+# osbornty@bc.edu
+# 09/15/2022
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('english/data/annotation/NYT_ENG_20131220.0283.best.xml')
+root = tree.getroot()
+
+relation_annotations = root[0][0]
+sentiment_annotations = root[0][1]
+for relation in relation_annotations:
+    print(relation.tag, relation.attrib)
+    for child in relation:
+        if child.tag == 'trigger':
+            print(child.tag, child.attrib, child.text)
+        else:
+            print(child[0].tag, child[0].attrib)
+    print('-------------------------------')
