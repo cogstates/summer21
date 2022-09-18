dcd611ce5f45b41dfec815cc777a6c86eac5b5d8
Tyler Osborne <tyler.922@hotmail.com>
2022-09-18 17:12:40 -0400

    working on loading BEST's source XML data into python data structures

---
+++ db_implementation/BEST/best2master_v1.py
@@ -0,0 +1,73 @@
+# author: tyler osborne
+# osbornty@bc.edu
+# 09/18/2022
+
+import xml.etree.ElementTree as ET
+import os
+
+
+class BEST2MASTER:
+
+    def __init__(self):
+        self.source_text = {}
+        self.ere = {}
+        self.best = {}
+        self.load_source_text()
+
+    # loading all source text
+
+    # source text will be stored as a dictionary of dictionaries,
+    # each inner dictionary representing one source XML file,
+    # accessed by its document ID. each entry inside a given inner dictionary
+    # will represent a discrete chunk of text as it is split up in the XML files.
+    # keys for these entries will either be taken directly from the associated XML entry,
+    # otherwise we will auto-generate them as ascending integers.
+    # headlines will always have an ID of 0 in cases where the XML does not give us IDs
+    def load_source_text(self):
+        directory = '../../raw_corpora/BEST/english/data/source/'
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            ext = os.path.splitext(f)[1]
+            if ext == '.xml':
+                self.parse_source_file(f)
+
+    # parsing a single source XML file
+    def parse_source_file(self, f):
+        tree = ET.parse(f)
+        root = tree.getroot()
+        metadata = root.attrib
+        doc_id = metadata['id']
+        self.source_text[doc_id] = {}
+
+        for child in root:
+            if child.tag == 'HEADLINE':
+                self.source_text[doc_id][0] = child.text
+            elif child.tag == 'TEXT':
+                passage_id = 1
+                for passage in child:
+                    self.source_text[doc_id][passage_id] = passage.text
+                    passage_id += 1
+            elif child.tag == 'post':
+                self.source_text[doc_id][child.attrib['id']] = child.text
+
+        # testing
+
+        for doc_id in self.source_text:
+            doc = self.source_text[doc_id]
+            print(doc_id)
+            if doc_id == "ENG_DF_000261_20150321_F00000081":
+                for p_id in doc:
+                    print(p_id, doc[p_id])
+                print('\n')
+
+    # loading ERE data
+    def load_ere(self):
+        pass
+
+    # loading BEST annotation data
+    def load_best(self):
+        pass
+
+
+if __name__ == "__main__":
+    test = BEST2MASTER()
