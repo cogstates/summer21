8e901f55891550b44d167152efe60c9bc6664d6b
Tyler Osborne <tyler.922@hotmail.com>
2022-11-27 14:10:54 -0500

    best progress

--- db_implementation/BEST/best2master_v1.py
+++ db_implementation/BEST/best2master_v1.py
@@ -51,29 +51,50 @@ class BEST2MASTER:
         raw_text_orig = f.read()
         tree = xmltodict.parse(raw_text_orig)['DOC']
         # self.pp.pprint(tree)
-        text = tree['TEXT']['P']
-        full_text = ' '.join(text)
-
-        tokenized_sentences = []
-        for sen in text:
-            output = sent_tokenize(sen)
-            paragraph = []
-            for s in output:
-                paragraph.append(s)
-            tokenized_sentences.append(paragraph)
+        # text = tree['TEXT']['P']
+        # full_text = ' '.join(text)
 
         # populating dictionary of paragraph start indicies "<P>"
         paragraph_indices = {}
         raw_text = raw_text_orig
-        p_start = raw_text.find('<P>')
+        p_start = raw_text.find('<P>\n')
 
         while p_start != -1:
-            p_end = raw_text.find('</P>', p_start)
+
+            p_end = raw_text.find('\n</P>', p_start)
             paragraph_indices[p_start] = p_end
-            p_start = raw_text.find('<P>', p_start + len('<P>'))
+            p_start = raw_text.find('<P>\n', p_start + len('<P>\n'))
+
+        p_starts = sorted(paragraph_indices.keys())
+        tokenized_sentences = []
+        i = 1
+
+        text = []
+        for p_start in p_starts:
+            p_end = paragraph_indices[p_start]
+            paragraph = raw_text_orig[p_start + len('<P>\n'):p_end]
+            text.append(paragraph)
+
+        for paragraph in text:
+
+            p_start = p_starts.pop(0)
+            sent_tokenized_paragraph = sent_tokenize(paragraph)
+            final_paragraph = []
+
+            for n, s in enumerate(sent_tokenized_paragraph):
+
+                sent_offset_start = raw_text_orig.index(s) - p_start
+                if n > 0:
+                    for s2 in sent_tokenized_paragraph[:n]:
+                        sent_offset_start += len(s2)
+
+                final_paragraph.append((i, sent_offset_start, s))
+                i += 1
+
+            tokenized_sentences.append(final_paragraph)
 
         entry = {'id': tree['@id'], 'headline': tree['HEADLINE'],
-                 'raw_text': raw_text_orig, 'full_text': full_text,
+                 'raw_text': raw_text_orig, 'split_text': text,
                  'sentences': tokenized_sentences,
                  'paragraph_indices': paragraph_indices}
         self.source_text[tree['@id']] = entry
@@ -87,36 +108,90 @@ class BEST2MASTER:
         tree = xmltodict.parse(f.read())['belief_sentiment_doc']
         source_id = f_name[f_name.index('annotation/') + len('annotation/'):f_name.index('.best.xml')]
 
+        source_text = self.source_text[source_id]
+        raw_source_text = source_text['raw_text']
+
         relation_belief_annotations = tree['belief_annotations']['relations']['relation']
         event_belief_annotations = tree['belief_annotations']['events']['event']
 
         final_relation_belief_annotations = []
+        # the structure of this list is as follows:
+        # list of lists containing one element per relation belief annotation
+        # inside each element (a list), we have,
+        # the relative mention ID at index 0,
+        # a series of 1-3 lists, each representing an atomic belief
+        # inside each belief list we have label, polarity, sarcasm,
+        # and source ere id, source offset, source length and source text (these are null if not present)
+        # then, after these 1-3 belief lists, we have one more list representing the trigger (offset, length, text);
+        # these are also null if there is no trigger
 
         for relation_belief_annotation in relation_belief_annotations:
-            ere_id = relation_belief_annotation['@ere_id']
+
+            beliefs = []
+            result = []
+
+            relm_ere_id = relation_belief_annotation['@ere_id']
             belief = relation_belief_annotation['beliefs']['belief']
+
+            result.append(relm_ere_id)
+
             if type(belief) != list:
-                label = belief['@type']
+                beliefs.append(belief)
+            else:
+                beliefs = belief
+
+            for b in beliefs:
+
+                this_belief = []
+                label = b['@type']
+
                 if label != 'na':
-                    polarity = belief['@polarity']
-                    if belief['@sarcasm'] == 'no':
+                    polarity = b['@polarity']
+                    if b['@sarcasm'] == 'no':
                         sarcasm = False
                     else:
                         sarcasm = True
                 else:
                     polarity, sarcasm = None, None
 
-                if 'trigger' in relation_belief_annotation:
-                    trigger = relation_belief_annotation['trigger']
-                    offset = int(trigger['@offset'])
-                    length = int(trigger['@length'])
-                    text = trigger['#text']
-                else:
-                    offset, length, text = None, None, None
+                this_belief.append(label)
+                this_belief.append(polarity)
+                this_belief.append(sarcasm)
 
-                final_relation_belief_annotation = [ere_id, text, offset, length]
-            elif len(belief) > self.max_length:
-                self.max_length = len(belief)
+                if 'source' in b:
+                    source = b['source']
+                    source_ere_id = source['@ere_id']
+                    source_offset = int(source['@offset'])
+                    source_length = int(source['@length'])
+                    source_text = source['#text']
+
+                    # assert raw_source_text[source_offset:source_offset + source_length] == source_text
+                else:
+                    source_ere_id, source_offset, source_length, source_text = None, None, None, None
+
+                this_belief.append(source_ere_id)
+                this_belief.append(source_offset)
+                this_belief.append(source_length)
+                this_belief.append(source_text)
+
+                result.append(this_belief)
+
+            if 'trigger' in relation_belief_annotation:
+                trigger = relation_belief_annotation['trigger']
+                trigger_offset = int(trigger['@offset'])
+                trigger_length = int(trigger['@length'])
+                trigger_text = trigger['#text']
+
+                # assert raw_source_text[trigger_offset:trigger_offset+trigger_length] == trigger_text
+            else:
+                trigger_offset, trigger_length, trigger_text = None, None, None
+
+            result.append([trigger_offset, trigger_length, trigger_text])
+
+            final_relation_belief_annotations.append(result)
+
+        self.best[source_id] = {'relation_belief_annotations': final_relation_belief_annotations}
+
 
 
 
@@ -129,7 +204,9 @@ if __name__ == "__main__":
     test.load_data()
     source_text_ids = [key for key in test.source_text.keys()]
     # test.pp.pprint(test.source_text[source_text_ids[0]])
-    print(source_text_ids[0])
-    test.pp.pprint((test.source_text[source_text_ids[0]]['paragraph_indices']))
+    print(source_text_ids[1])
+    # test.pp.pprint((test.source_text[source_text_ids[1]]['split_text']))
+    test.pp.pprint((test.source_text[source_text_ids[1]]['sentences']))
+    # test.pp.pprint((test.best[source_text_ids[1]]['relation_belief_annotations']))
     # print(test.best[source_text_ids[0]]['max_offset'])
     # print(len(test.source_text[source_text_ids[0]]['full_text']) + len(test.source_text[source_text_ids[0]]['headline']))
